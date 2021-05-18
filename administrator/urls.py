c9311b1c86b4d301bd33af68b6c3a79bc2eeaa4d
Massimiliano <massimiliano.porzio@gmail.com>
2021-05-18 18:25:59 +0200

    Admin API finita

--- administrator/urls.py
+++ administrator/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path,include
 
-from .views import AmbassadorAPIView,ProductGenericAPIView
+from .views import AmbassadorAPIView, ProductGenericAPIView, LinkAPIView, OrderAPIView
 
 urlpatterns = [
    path('',include('common.urls')),
    path('ambassadors',AmbassadorAPIView.as_view()),
    path('products',ProductGenericAPIView.as_view()), #senza chiave primaria
    path('products/<str:pk>',ProductGenericAPIView.as_view()), #senza chiave primaria
+   path('users/<str:pk>/links',LinkAPIView.as_view()), #GET i lINK associati all'user
+   path('orders', OrderAPIView.as_view()), #ordini da processare
 
 ]
--- administrator/views.py
+++ administrator/views.py
@@ -6,10 +6,10 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from administrator.serializers import ProductSerializer
+from administrator.serializers import ProductSerializer, LinkSerializer, OrderSerializer
 from common.authentication import JWTAuthentication
 from common.serializers import UserSerializer
-from core.models import User, Product
+from core.models import User, Product, Link, Order
 
 
 class AmbassadorAPIView(APIView):
@@ -46,4 +46,24 @@ class ProductGenericAPIView(generics.GenericAPIView,mixins.RetrieveModelMixin,mi
     def delete(self, request, pk=None):
         response = self.destroy(request, pk)
         return response
+
+
+class LinkAPIView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, pk=None):
+        links = Link.objects.filter(user_id=pk)
+        serializer = LinkSerializer(links, many=True)
+        return Response(serializer.data)
+
+
+class OrderAPIView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        orders = Order.objects.filter(complete=True)
+        serializer = OrderSerializer(orders, many=True)
+        return Response(serializer.data)
 
